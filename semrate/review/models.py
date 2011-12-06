f15ad6cda439f1fe9d4bbff1ae5d9668c4507a1b
mrvelle <mrvelle@6d15b07a-e192-5f77-10af-f204dfca5002>
2011-12-06 09:54:35 +0000

    Tag addition instant-ajaxified

--- semrate/review/models.py
+++ semrate/review/models.py
@@ -14,17 +14,22 @@ class Product(models.Model):
 
 class Parameter(models.Model):
   name = models.CharField(max_length=32)
+  # "simple name", 
+  #the name with _ and lowercase, used for ids and stuff
+  sname = models.CharField(max_length=32) 
   description = models.TextField()
   product = models.ForeignKey(Product)
   def __str__(self):
-    return self.name
+    return '%s - %s' % (self.product.name, self.name)
   def get_tags(self):
     return Tag.objects.filter(parameter = self)
   def get_tags_neg(self):
     return Tag.objects.filter(parameter = self, charge=-1)
   def get_tags_pos(self):
     return Tag.objects.filter(parameter = self, charge=1)
-
+  def clean_fields(self, exclude=[]):
+    self.name = self.name.strip()
+    self.sname = self.name.replace(' ','_').lower()
 
 class Tag(models.Model):
   tagtext = models.CharField(max_length=32)
@@ -36,18 +41,25 @@ class Tag(models.Model):
 class ProductForm(forms.ModelForm):
   class Meta:
     model = Product
-    exclude = ['suburi']
+    exclude = ['suburi',]
 
 class ParameterForm(forms.ModelForm):
   class Meta:
     model = Parameter
-    #exclude = ('product',)
+    exclude = ('sname',)
+    #fields = ('name','description')
+    # I dont get it, when I exclude sname, I also can't use name parameterform.name in the template
   description = forms.CharField(widget=forms.Textarea(attrs={'cols': 25, 'rows': 2}))
   
 class TagForm(forms.ModelForm):
   class Meta:
     model = Tag
+  #tagtext = forms.TextField(widget=forms.TextInput(attrs={'class':'taginput'}))
+  tagtext = forms.CharField(widget=forms.TextInput(attrs={'class':'taginput', 'value':''}))
 
 
 
 # Create your models here.
+
+
+
