2e9f82ed73fb1311ec2599fdbd6d71ac497c5be0
aladagemre <aladagemre@6d15b07a-e192-5f77-10af-f204dfca5002>
2011-12-06 06:05:49 +0000

    Flexible db path

--- semrate/settings.py
+++ semrate/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '%s/db.sqlite3' % PROJECT_PATH, # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
