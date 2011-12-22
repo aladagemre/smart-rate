5d04ac51f6cb7e1606021122bdc946ddf32004b0
aladagemre <aladagemre@6d15b07a-e192-5f77-10af-f204dfca5002>
2011-12-22 20:51:52 +0000

    * Parameters are associated with CategoryParameter which is an abstract listing. * When we create a parameter for a cell phone, all other cell phones get that new parameter once they are viewed. * Category can be created * Categories can be listed * Products of a category can be listed

--- semrate/review/views.py
+++ semrate/review/views.py
@@ -5,7 +5,9 @@ from django.template.loader import get_template
 from django.core.context_processors import csrf
 from django.db.models import Q
 from django.shortcuts import render_to_response, redirect
-from models import Product, ProductForm, Parameter, ParameterForm, Tag, TagForm
+from models import * #Product, ProductForm, Parameter, Category, CategoryParameter, CategoryParameterForm, Tag, TagForm
+import copy
+from django.template.defaultfilters import slugify
 
 def index(request):
   t = get_template('index.html')
@@ -51,12 +53,28 @@ def viewproduct(request, path):
   product = Product.objects.get(suburi=path)
   parameters = Parameter.objects.filter(product=product)
 
+  parameter_c_values = set(map(lambda p: p.category_parameter, parameters))
+  category_parameters = set(CategoryParameter.objects.filter(category=product.category))
+  
+  lacking_c_parameters = category_parameters - parameter_c_values
+  
+  
+  if lacking_c_parameters:
+    new_parameters = []
+    for c_param in lacking_c_parameters:
+      slug = slugify(c_param.name)
+      p = Parameter(sname=slug, category_parameter=c_param, product=product, score_total=0, score_count=0)
+      p.save()
+  
+    parameters = Parameter.objects.filter(product=product)
+
+	  
   t = get_template('viewproduct.html')
   c = RequestContext(request,{})
   c['request'] = request
   c['product'] = product
   c['parameters'] = parameters
-  c['parameterform'] = ParameterForm()
+  c['parameterform'] = CategoryParameterForm()
   c['tagform'] = TagForm()
   c.update(csrf(request))
   return  HttpResponse(t.render(c))
@@ -71,13 +89,25 @@ def rate_parameter(request):
   product = parameter.product
   
   return redirect('/products/%s' % product.suburi)
-  
+"""  
 def ajax_createparameter(request):
-  parameterform = ParameterForm(request.POST)
+  parameterform = CategoryParameterForm(request.POST)
   stuff = dir(parameterform)
   errors = parameterform.errors
   parameterform.save()
   return HttpResponse('success')
+"""
+def create_parameter(request):
+  product = Product.objects.get(id=int(request.POST.get('product')))
+  d = copy.deepcopy(request.POST)
+  del d['product']
+  parameterform = CategoryParameterForm(d)
+  stuff = dir(parameterform)
+  errors = parameterform.errors
+  parameterform.save()
+  
+  return redirect('/products/%s' % product.suburi)
+
 
 def ajax_createtag(request):
   tagform = TagForm(request.POST)
@@ -90,3 +120,38 @@ def ajax_createtag(request):
   return HttpResponse('success')
 
 
+
+def newcategory(request):
+	if 'submit' in request.POST:
+		slug = slugify(request.POST['name'])
+		d = copy.deepcopy(request.POST)
+		d['slug'] = slug
+		categoryform = CategoryForm(d)
+		if categoryform.is_valid():
+			c = categoryform.save()
+			return HttpResponseRedirect('/categories')
+
+	else:
+		categoryform = CategoryForm()
+	
+	t = get_template('newcategory.html')
+	c = RequestContext(request,{})
+	c['categoryform'] = categoryform
+	c['request'] = request
+	c.update(csrf(request))
+	return  HttpResponse(t.render(c))
+	
+def categories(request):
+	categories = Category.objects.all()
+	return render_to_response("categories.html", {
+		'categories': categories,
+	})
+def category(request, slug):
+	category = Category.objects.get(slug=slug)
+	products = Product.objects.filter(category=category)
+	
+	return render_to_response("products_of_category.html", {
+		'category': category,
+		'products': products,
+	})
+	
