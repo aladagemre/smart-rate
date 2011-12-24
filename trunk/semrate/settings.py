e353bb893804fe01d99712040d28e1a5f469c6c4
aladagemre <aladagemre@6d15b07a-e192-5f77-10af-f204dfca5002>
2011-12-24 23:04:28 +0000

    * Messaging tryout

--- trunk/semrate/settings.py
+++ trunk/semrate/settings.py
@@ -41,7 +41,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Istanbul'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -148,6 +148,7 @@ INSTALLED_APPS = (
     'review',
     'registration',
     'member',
+    'django_messages',
 )
 
 # A sample logging configuration. The only tangible logging
